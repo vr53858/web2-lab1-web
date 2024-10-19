c42c37a86d7ad654553066fc890a546fbc94af0d
Vlatko Rohak <vlatko.rohak@eccos.com.hr>
2024-10-19 23:47:04 +0200

    added auth0 OIDC

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Application, Request, Response } from 'express';
+const { requiresAuth } = require('express-openid-connect');
+
 import dotenv from 'dotenv';
 import ticketRoutes from './routes/ticketRoutes';
 import axios from 'axios';
 import path from 'path';
+const { auth } = require('express-openid-connect');
 
 dotenv.config();
 
@@ -11,10 +14,25 @@ const app : Application = express();
 const PORT = process.env.PORT || 3000;
 const API_URL = process.env.API_URL;
 
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+  };
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../src/views'));
 
+app.use(auth(config));
+
 app.use(express.json());
+
+app.get('/', (req : any, res : Response) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
 
 app.get('/', async (req: Request, res: Response) => {
     try {
@@ -26,6 +44,10 @@ app.get('/', async (req: Request, res: Response) => {
         res.status(500).send(`Error fetching ticket count: ${(error as Error).message}`);
     }
 });
+
+app.get('/profile', requiresAuth(), (req : any, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
 
 app.use('/ticket', ticketRoutes);
 
